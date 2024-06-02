041c4b33a68470de0859ddf94112ce587b28281c
shahidullah177 <mdshahidsumon177@gmail.com>
2024-06-02 11:38:33 +0600

    Slider

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Banner from "./Banner";
 
 const Home = () => {
     return (
-        <div className="">
+        <div className="px-4 lg:mx-24">
             <Banner></Banner>
         </div>
     );
