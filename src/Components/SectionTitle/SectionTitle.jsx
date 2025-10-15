97a6cd95073bbf9352c776800f592d5eb2774429
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-16 04:19:06 +0600

    code refector

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,12 +1,10 @@
-
-
-const SectionTitle = ({heading, description}) => {
-    return (
-        <div className="my-10 p-5 space-y-3 text-center flex flex-col items-center">
-            <h1 className="text-5xl font-roboto font-bold">{heading}</h1>
-            <p className="text-gray-600 w-7/12  ">{description}</p>
-        </div>
-    );
+const SectionTitle = ({ heading, description }) => {
+  return (
+    <div className="my-10 p-5 space-y-3 text-center flex flex-col items-center">
+      <h1 className="text-5xl font-roboto font-bold">{heading}</h1>
+      <p className="text-gray-600 w-7/12  ">{description}</p>
+    </div>
+  );
 };
 
 export default SectionTitle;
