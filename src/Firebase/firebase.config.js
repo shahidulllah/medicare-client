4dcc8367dd8a8d732bf9513fec066e27672cd8e3
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-16 04:20:34 +0600

    code refector

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,18 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId:  import.meta.env.VITE_PROJECTID,
+  projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  appId: import.meta.env.VITE_APPID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const Auth = getAuth(app);
 export default Auth;
