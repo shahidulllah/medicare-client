fc55b44a08c1b1fa6eb1a4483d0a51bb7a2d125e
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-17 19:54:27 +0600

    just more

--- src/Hooks/useUser.jsx
+++ src/Hooks/useUser.jsx
@@ -3,20 +3,19 @@ import useAxiosPublic from "./useAxiosPublic";
 import { AuthContext } from "../Components/AuthProvider/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
 
-
 const useUser = () => {
-    const axiosPublic = useAxiosPublic()
-    const { user } = useContext(AuthContext);
+  const axiosPublic = useAxiosPublic();
+  const { user } = useContext(AuthContext);
 
-    const { data: User, refetch } = useQuery({
-        queryKey: [user?.email, 'user'],
-        queryFn: async () => {
-            const res = await axiosPublic.get(`/users/${user.email}`);
-            console.log(res.data)
-            return res.data;
-        }
-    })
-    return {User, refetch};
+  const { data: User, refetch } = useQuery({
+    queryKey: [user?.email, "user"],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/users/${user.email}`);
+      console.log(res.data);
+      return res.data;
+    },
+  });
+  return { User, refetch };
 };
 
 export default useUser;
