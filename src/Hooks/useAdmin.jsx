403b5fb9f69392952e689023bd9d1a28a617aa1c
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-17 19:52:33 +0600

    code decorate

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -3,19 +3,18 @@ import { AuthContext } from "../Components/AuthProvider/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
 
-
 const useAdmin = () => {
-    const axiosSecure = useAxiosSecure()
-    const { user } = useContext(AuthContext);
-    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
-        queryKey: [user?.email, 'isAdmin'],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/users/admin/${user.email}`);
-            console.log(res.data)
-            return res.data?.admin;
-        }
-    })
-    return [isAdmin, isAdminLoading];
+  const axiosSecure = useAxiosSecure();
+  const { user } = useContext(AuthContext);
+  const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+    queryKey: [user?.email, "isAdmin"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/admin/${user.email}`);
+      console.log(res.data);
+      return res.data?.admin;
+    },
+  });
+  return [isAdmin, isAdminLoading];
 };
 
 export default useAdmin;
