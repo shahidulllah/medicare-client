5830308e15f6b3e7bcb9d342d5d9dacf669faf7d
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-17 19:53:29 +0600

    more decoration

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    // baseURL:"https://ass12-medicare-server.vercel.app"
-    baseURL:"http://localhost:5000"
-   
-    
-})
+  // baseURL:"https://ass12-medicare-server.vercel.app"
+  baseURL: "http://localhost:5000",
+});
 const useAxiosPublic = () => {
-    return axiosPublic;
+  return axiosPublic;
 };
 
 export default useAxiosPublic;
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,38 +4,42 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Components/AuthProvider/AuthProvider";
 
 const axiosSecure = axios.create({
-    // baseURL:"https://ass12-medicare-server.vercel.app"
-    baseURL:"http://localhost:5000"
-   
-    
-}) 
+  // baseURL:"https://ass12-medicare-server.vercel.app"
+  baseURL: "http://localhost:5000",
+});
 const useAxiosSecure = () => {
-    const navigate = useNavigate();
-    const {logOut} = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { logOut } = useContext(AuthContext);
 
-    axiosSecure.interceptors.request.use(function(config){
-        const token = localStorage.getItem('access-token')
-        // console.log('request stop by interceptor', token)
-        config.headers.authorization = `Bearer ${token}`;
-        return config;
-    }, function(error){
-        return Promise.reject(error)
-    })
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      // console.log('request stop by interceptor', token)
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
 
-    // intercepts 401 and 403
-    axiosSecure.interceptors.response.use(function(response){
-        return response;
-    }, async (error)=>{
-        const status = error.response.status;
-        // console.log('staus code error', status);
-        if(status === 401 || status === 403){
-            await logOut();
-            navigate('/login')
-        }
-        return Promise.reject(error);
-    })
+  // intercepts 401 and 403
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      // console.log('staus code error', status);
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
 
-   return axiosSecure;
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
