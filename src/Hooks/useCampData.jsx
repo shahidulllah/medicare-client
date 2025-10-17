1c91bced272379077be43682a1fd8d7c4a992a13
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-17 19:54:05 +0600

    working

--- src/Hooks/useCampData.jsx
+++ src/Hooks/useCampData.jsx
@@ -3,20 +3,18 @@ import useAxiosSecure from "./useAxiosSecure";
 import { useContext } from "react";
 import { AuthContext } from "../Components/AuthProvider/AuthProvider";
 
+const useCampData = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useContext(AuthContext);
 
-
-const useCampData = () =>{
-    const axiosSecure = useAxiosSecure();
-    const {user} = useContext(AuthContext);
-
-   const {refetch, data: campData = []} = useQuery({
-        queryKey: ['camp', user?.email],
-        queryFn: async () =>{
-            const res = await axiosSecure.get(`/camps`)
-            return res.data.camps;
-        }
-   })
-  return[campData,refetch]
+  const { refetch, data: campData = [] } = useQuery({
+    queryKey: ["camp", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/camps`);
+      return res.data.camps;
+    },
+  });
+  return [campData, refetch];
 };
 
 export default useCampData;
--- src/Hooks/useParticipantData.jsx
+++ src/Hooks/useParticipantData.jsx
@@ -3,19 +3,18 @@ import { AuthContext } from "../Components/AuthProvider/AuthProvider";
 import useAxiosSecure from "./useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 
-
 const useParticipantData = () => {
-    const axiosSecure = useAxiosSecure();
-    const {user} = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+  const { user } = useContext(AuthContext);
 
-   const {refetch, data: participantData = []} = useQuery({
-        queryKey: ['participantInfo', user?.email],
-        queryFn: async () =>{
-            const res = await axiosSecure.get(`/participants`)
-            return res.data;
-        }
-   })
-  return[participantData,refetch]
+  const { refetch, data: participantData = [] } = useQuery({
+    queryKey: ["participantInfo", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/participants`);
+      return res.data;
+    },
+  });
+  return [participantData, refetch];
 };
 
 export default useParticipantData;
