796e6f9cf7a6ef5fa98a7adaa958b6524d2ae331
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-19 17:51:01 +0600

    working

--- src/Pages/Dashboard/UserDashboard/Payment.jsx
+++ src/Pages/Dashboard/UserDashboard/Payment.jsx
@@ -3,26 +3,23 @@ import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import CheckoutForm from "./CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
 
-
 const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATWAY_PK);
 const Payment = () => {
-    return (
-        <div>
-            <div className="text-green-600">
-                <SectionTitle heading={'Payment'}></SectionTitle>
-            </div>
+  return (
+    <div>
+      <div className="text-green-600">
+        <SectionTitle heading={"Payment"}></SectionTitle>
+      </div>
 
-
-
-            <div className="px-16">
-                <div className="shadow-2xl p-12 bg-green-100 rounded-xl">
-                    <Elements stripe={stripePromise}>
-                        <CheckoutForm></CheckoutForm>
-                    </Elements>
-                </div>
-            </div>
+      <div className="px-16">
+        <div className="shadow-2xl p-12 bg-green-100 rounded-xl">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm></CheckoutForm>
+          </Elements>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
--- src/Pages/Dashboard/UserDashboard/PaymentHistory.jsx
+++ src/Pages/Dashboard/UserDashboard/PaymentHistory.jsx
@@ -3,73 +3,78 @@ import { AuthContext } from "../../../Components/AuthProvider/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
-
-
 const PaymentHistory = () => {
-    const { user } = useContext(AuthContext);
-    const axiosSecure = useAxiosSecure()
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
 
-    const { data: payments = [] } = useQuery({
-        queryKey: ['payments', user.email],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/payments`)
-            return res.data;
-        }
-    })
-   
-    return (
-        <div className="p-4  lg:p-12 w-full flex justify-center">
-            <div className="container p-4 lg:p-10 mx-auto text-blue-400 bg-gray-200 rounded-lg">
-                <h2 className="mb-5 text-3xl text-center font-semibold text-green-600 leading-tight  -mt-6">Payment History ({payments.length})</h2>
-                <div className="overflow-x-auto">
-                    <table className="min-w-full text-sm">
-                        <colgroup>
-                            <col />
-                            <col />
-                            <col />
-                            <col />
-                            <col />
-                        </colgroup>
-                        <thead className="bg-green-700">
-                            <tr className="text-center font-bold text-slate-300">
-                                <th className="p-3 text-left">Camp Name</th>
-                                <th className="p-3">Camp Fees</th>
-                                <th className="p-3">Payment Status</th>
-                                <th className="p-3">Confirmation Status</th>
-                                <th className="p-3">Date</th>
-                                <th className="p-3 ">Transaction ID</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                payments.map(pay => <tr key={pay._id} className="border-b border-opacity-20 border-green-700 bg-gray-900">
-                                    <td className="p-3 text-left">
-                                        <p className="text-purple-500 font-semibold">{pay.CampName}</p>
-                                    </td>
-                                    <td className="p-3 text-center">
-                                        <p>$ {pay.CampFees}</p>
-                                    </td>
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments`);
+      return res.data;
+    },
+  });
 
-                                    <td className="p-3 text-center">
-                                    <p>{pay.PaymentStatus}</p>
-                                    </td>
-                                    <td className="p-3 text-center">
-                                        <p>{pay.ConfirmationStatus}</p>
-                                    </td>
-                                    <td className="p-3 text-center">
-                                        <p>{pay.Date}</p>
-                                    </td>
-                                    <td className="p-3 text-center">
-                                        <p>{pay.TransactionId}</p>
-                                    </td>
-                                </tr>)
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  return (
+    <div className="p-4  lg:p-12 w-full flex justify-center">
+      <div className="container p-4 lg:p-10 mx-auto text-blue-400 bg-gray-200 rounded-lg">
+        <h2 className="mb-5 text-3xl text-center font-semibold text-green-600 leading-tight  -mt-6">
+          Payment History ({payments.length})
+        </h2>
+        <div className="overflow-x-auto">
+          <table className="min-w-full text-sm">
+            <colgroup>
+              <col />
+              <col />
+              <col />
+              <col />
+              <col />
+            </colgroup>
+            <thead className="bg-green-700">
+              <tr className="text-center font-bold text-slate-300">
+                <th className="p-3 text-left">Camp Name</th>
+                <th className="p-3">Camp Fees</th>
+                <th className="p-3">Payment Status</th>
+                <th className="p-3">Confirmation Status</th>
+                <th className="p-3">Date</th>
+                <th className="p-3 ">Transaction ID</th>
+              </tr>
+            </thead>
+            <tbody>
+              {payments.map((pay) => (
+                <tr
+                  key={pay._id}
+                  className="border-b border-opacity-20 border-green-700 bg-gray-900"
+                >
+                  <td className="p-3 text-left">
+                    <p className="text-purple-500 font-semibold">
+                      {pay.CampName}
+                    </p>
+                  </td>
+                  <td className="p-3 text-center">
+                    <p>$ {pay.CampFees}</p>
+                  </td>
+
+                  <td className="p-3 text-center">
+                    <p>{pay.PaymentStatus}</p>
+                  </td>
+                  <td className="p-3 text-center">
+                    <p>{pay.ConfirmationStatus}</p>
+                  </td>
+                  <td className="p-3 text-center">
+                    <p>{pay.Date}</p>
+                  </td>
+                  <td className="p-3 text-center">
+                    <p>{pay.TransactionId}</p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PaymentHistory;
