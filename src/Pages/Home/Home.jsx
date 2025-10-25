58a868038e7b71c737921b70187f30b5185c5e54
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-25 21:27:08 +0600

    working

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import Banner from "./Banner";
 import Camps from "./Camps";
 
-
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Camps></Camps>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Camps></Camps>
+    </div>
+  );
 };
 
 export default Home;
