0bbb983bc6aad72ebaf5a10450526442a5604cbd
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-27 20:28:59 +0600

    working

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -2,17 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Home/Navbar";
 import Footer from "../Pages/Home/Footer";
 
-
-
 const Root = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
