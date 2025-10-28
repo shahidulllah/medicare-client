ff2674a6b4e15e21bc06bab0b5842c3fbc75af2f
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-28 18:35:15 +0600

    working

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -3,20 +3,23 @@ import useAdmin from "../Hooks/useAdmin";
 import { AuthContext } from "../Components/AuthProvider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
+const AdminRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const [isAdmin, isAdminLoading] = useAdmin();
+  const location = useLocation();
 
-const AdminRoute = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
-    const [isAdmin, isAdminLoading] = useAdmin();
-    const location = useLocation();
+  if (loading || isAdminLoading) {
+    return (
+      <div className="text-center p-16">
+        <span className=" loading loading-spinner text-primary"></span>
+      </div>
+    );
+  }
 
-    if(loading || isAdminLoading){
-        return <div className="text-center p-16"><span className=" loading loading-spinner text-primary"></span></div>
-    }
-
-    if(user && isAdmin){
-        return children;
-    }
-    return <Navigate  to='/' state={{from: location}} replace></Navigate>;
+  if (user && isAdmin) {
+    return children;
+  }
+  return <Navigate to="/" state={{ from: location }} replace></Navigate>;
 };
 
 export default AdminRoute;
