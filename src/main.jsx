f7c281572d607f124bf3674fb082cc14d66aca57
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-29 20:26:45 +0600

    working

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import AuthProvider from './Components/AuthProvider/AuthProvider'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routes/Route'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AuthProvider from "./Components/AuthProvider/AuthProvider";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Route";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
